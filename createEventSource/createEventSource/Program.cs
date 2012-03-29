be1b31286cb5fc7e5e2e475b2f4ab9dd43d26241
yasinanbar <yasin.anbar@dotto.com.tr>
2012-03-29 15:35:41 +0300

    my hola mundo changes

---
+++ createEventSource/createEventSource/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace createEventSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(writeErr());
+            Console.ReadLine();
+        }
+
+        public static string writeErr()
+        {
+            string source = "Eimza";
+            if (!EventLog.SourceExists(source))
+            {
+                System.Diagnostics.Debug.WriteLine("asdfasdfasdf");
+                EventLog.CreateEventSource(source, "HATA");
+                return "createe";
+            }
+
+            EventLog.WriteEntry(source, "Hata Mesajı Denem", EventLogEntryType.Error);
+            return "yazdı";
+        }
+    }
+}
